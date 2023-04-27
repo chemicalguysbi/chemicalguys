8e716e18a0d949a98693bb78d585c86b4392c4a2
itadmin@chemicalguys.com <itadmin@chemicalguys.com>
2023-04-27 04:37:43 +0000

    source data changes

--- models/staging_zone/sales_orders_party_site.sql
+++ models/staging_zone/sales_orders_party_site.sql
@@ -7,7 +7,7 @@
 with sales_orders_party_site as (
 SELECT *
 FROM
-  cg-gbq-p.oracle_fusion_fscm_scmextract_doobiccextract.order_address_extract_pvo
+  cg-gbq-p.oracle_fusion_fscm_partiesanalytics.party_site
 )
 
 select *, current_datetime() as load_date_time from sales_orders_party_site
